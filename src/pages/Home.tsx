ddbe5c2064440f8c8908b7bb21f3a263a11a9e28
rodriggo <rodrygolacort@hotmail.com>
2021-08-06 03:12:10 -0300

    autenticação terminada e inicializando cadastro

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import illustrationImg from '../assets/imagens/illustration.svg';
 import logoImg from '../assets/imagens/logo.svg';
 import googleIconImg from '../assets/imagens/google-icon.svg';
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.css';
+
 export function Home() {
     return (
-      <div> 
+      <div id="page-auth"> 
         <aside>
           <img src={illustrationImg} alt="ilustração simbolisando pertuntas e respostas" />
           <strong>Crie salas de Q&amp;A ao-vivo</strong>
@@ -11,21 +16,21 @@ export function Home() {
         </aside>
 
         <main>
-          <div>
+          <div className="main-content">
             <img src={logoImg} alt="logo" />
-            <button>
+            <button className="create-room">
               <img src={googleIconImg} alt="icone para entrar com conta google" />
               Crie sua sala com o Goole
             </button>
-            <div>ou entre em uma sala</div>
+            <div className="separator">ou entre em uma sala</div>
             <form>
               <input
                 type="text"
                 placeholder="Digite o codigo da sala"
               />
-              <button type="submit">
+              <Button type="submit">
                 Entrar na sala 
-              </button> 
+              </Button> 
             </form>
           </div>
         </main>
